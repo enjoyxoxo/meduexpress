44d04355b7c3a7dce01ee50d169823acb311a62d
Asante <asante0277@gmail.com>
2023-02-16 13:41:08 -0800

    Updated Filter Sort Grid

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -165,18 +165,29 @@ const OurStore = () => {
           </div>
           <div className="col-9">
             <div className="filter-sort-grid">
-              <div className="d-flex align-items-center gap-10">
-                <p className="mb-0">Sort By:</p>
-                <select name="" id="" className="form-control form-select">
-                  <option value="manual">Featured</option>
-                  <option value="best-selling" selected="selected">Best Selling</option>
-                  <option value="title-ascending">Alphabetically, A-Z</option>
-                  <option value="title-descending">Alphabetically, A-Z</option>
-                  <option value="price-ascending">Price, low to high</option>
-                  <option value="price-descending">Price, high to low</option>
-                  <option value="created-ascending">Date, old to new</option>
-                  <option value="created-descending">Date, new to old</option>
-                </select>
+              <div className="d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center gap-10">
+                  <p className="mb-0 d-block" style={{width: "100px"}}>Sort By:</p>
+                  <select name="" id="" className="form-control form-select">
+                    <option value="manual">Featured</option>
+                    <option value="best-selling" selected="selected">Best Selling</option>
+                    <option value="title-ascending">Alphabetically, A-Z</option>
+                    <option value="title-descending">Alphabetically, A-Z</option>
+                    <option value="price-ascending">Price, low to high</option>
+                    <option value="price-descending">Price, high to low</option>
+                    <option value="created-ascending">Date, old to new</option>
+                    <option value="created-descending">Date, new to old</option>
+                  </select>
+                </div>
+                <div className='d-flex align-items-center gap-10'>
+                  <p className='totalproducts mb-0'>21 products</p>
+                  <div className="d-flex gap-10 align-items-center grid">
+                    <img src="images/gr4.svg" className="d-block img-fluid" alt="grid" />
+                    <img src="images/gr3.svg" className="d-block img-fluid" alt="grid" />
+                    <img src="images/gr2.svg" className="d-block img-fluid" alt="grid" />
+                    <img src="images/gr.svg" className="d-block img-fluid" alt="grid" />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
