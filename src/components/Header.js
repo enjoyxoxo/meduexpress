4a6ee4dbb3e17eb0186e4bdedecab720b874427e
Asante <asante0277@gmail.com>
2023-02-16 13:00:18 -0800

    Shop By categories, Filter By, Price, Colors, Size, Product Tags, Random Product. All left sidebar created in the OurStore Component

--- src/components/Header.js
+++ src/components/Header.js
@@ -84,7 +84,7 @@ const Header = () => {
               <div className="menu-links">
                 <div className="d-flex align-items-center gap-15">
                   <NavLink to="/">Home</NavLink>
-                  <NavLink to="/">Our Store</NavLink>
+                  <NavLink to="/store">Our Store</NavLink>
                   <NavLink to="/blogs">Blogs</NavLink>
                   <NavLink to="/contact">Contact</NavLink>
                 </div>
@@ -97,4 +97,4 @@ const Header = () => {
   </>
 }
 
-export default Header
+export default Header;
