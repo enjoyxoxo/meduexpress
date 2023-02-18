cfa9e52272a0a871616c1f4e28e4cc67461fcb0e
Asante <asante0277@gmail.com>
2023-02-17 21:35:29 -0800

    Forgotpassword, Login, resetpassword and signup all completed

--- src/App.js
+++ src/App.js
@@ -6,8 +6,12 @@ import About from './pages/About';
 import Blog from './pages/Blog';
 import CompareProduct from './pages/CompareProduct';
 import Contact from './pages/Contact';
+import Forgotpassword from './pages/Forgotpassword';
 import Home from './pages/Home';
+import Login from './pages/Login';
 import OurStore from './pages/OurStore';
+import Resetpassword from './pages/Resetpassword';
+import Signup from './pages/Signup';
 import Wishlist from './pages/Wishlist';
 
 function App() {
@@ -22,6 +26,10 @@ function App() {
           <Route path="blog" element={<Blog />} />
           <Route path="compare-product" element={<CompareProduct />} />
           <Route path="wishlist" element={<Wishlist />} />
+          <Route path="login" element={<Login />} />
+          <Route path="forgot-password" element={<Forgotpassword />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="reset-password" element={<Resetpassword />} />
         </Route>
       </Routes>
     </BrowserRouter>
